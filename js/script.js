c1d2f139a729ccc6a2f56eeb65b9d2a0627ea192
beratukiqi <berat.u@live.se>
2023-02-28 15:47:58 +0100

    Smaller player

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+
+// SPOTIFY Api embedding.
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
     let element = document.getElementById('embed-iframe');
     let options = {
         height: '400',
-        width: '60%',
-        uri: 'spotify:artist:0hEurMDQu99nJRq8pTxO14'
+        width: '40%',
+        uri: 'spotify:track:4T6FWA703h6H7zk1FoSARw'
       };
     let callback = (EmbedController) => {};
     IFrameAPI.createController(element, options, callback);
   };
 
+
+  
